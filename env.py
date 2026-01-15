6c7b8ee84f9327249f919b29ed12d98b191c9c04
Rob-C-89 <robbiecornell@gmail.com>
2026-01-15 14:33:22 +0000

    Retry pushing env.py

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_UryumSBf6P3d@ep-wandering-poetry-agc78b9e.c-2.eu-central-1.aws.neon.tech/hub_polka_colt_562536")
+os.environ.setdefault(
+    "SECRET_KEY", "L<YreA82Xx$cYg=eDKIbOl2vjK^:NF<A"
+)
