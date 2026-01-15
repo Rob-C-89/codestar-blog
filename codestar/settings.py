32ae78683fd54c3e1cba0ebddd34ce2ca914771a
Rob-C-89 <robbiecornell@gmail.com>
2026-01-15 11:51:31 +0000

    Debug to false, Heroku added to allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0$^*ax4qrg*^#zets&k848dkx8s+*@ar%kk1+ke=jl(z75cr-2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 '127.0.0.1',]
 
 # Application definition
 
