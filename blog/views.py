af41f6f70fb52a3f02f4f6870c43d25dceeea75c
Rob-C-89 <robbiecornell@gmail.com>
2026-01-19 14:36:01 +0000

    Add postlist methods and comments

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,13 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     # model = Post
-    queryset = Post.objects.all()
+
+    # Filter by author ID
+    # queryset = Post.objects.filter(author=4)
+
+    # Display in order of time created (add "-" to reverse)
+    # queryset = Post.objects.all().order_by("created_on")
+
+    # Filter by Published status, i.e. filter out Drafts
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
