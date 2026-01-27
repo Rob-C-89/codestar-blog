0efd3b1d90627f3bffc0f19646b64adba6c9c4ab
Rob-C-89 <robbiecornell@gmail.com>
2026-01-27 15:13:54 +0000

    Fix about profile image bug

--- about/views.py
+++ about/views.py
@@ -5,17 +5,15 @@ from .forms import CollaborateForm
 
 
 def about_me(request):
-    """
-    Renders the About page
-    """
+
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
             collaborate_form.save()
             messages.add_message(
-                request, messages.SUCCESS,
-                'Collaboration request received! '
-                'I endeavour to respond within 2 working days.'
+                request, 
+                messages.SUCCESS,
+                "Collaboration request received! I endeavour to respond within 2 working days."
             )
 
     about = About.objects.all().order_by('-updated_on').first()
@@ -24,6 +22,8 @@ def about_me(request):
     return render(
         request,
         "about/about.html",
-        {"about": about,
-         "collaborate_form": collaborate_form},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        },
     )
--- env.py
+++ env.py
@@ -1,14 +1,8 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_UryumSBf6P3d@e"
-    "p-wandering-p"
-    "oetry-agc78b9e.c-2.eu-central-1.aws.neon.tech/hub_polka_colt_562536"
-    )
+    "DATABASE_URL", "postgresql://neondb_owner:npg_UryumSBf6P3d@ep-wandering-poetry-agc78b9e.c-2.eu-central-1.aws.neon.tech/hub_polka_colt_562536")
 os.environ.setdefault(
-    "SECRET_KEY", "L<YreA82Xx$cYg=eDKIbOl2vjK^:NF<A"
-    )
+    "SECRET_KEY", "L<YreA82Xx$cYg=eDKIbOl2vjK^:NF<A")
 os.environ.setdefault(
-    "CLOUDINARY_URL", "cloudinary://633512268544298:gbJdjVhgm1COKOwXF"
-    "ut841gRap8@dfpeg0tdy"
-    )
+    "CLOUDINARY_URL", "cloudinary://633512268544298:gbJdjVhgm1COKOwXFut841gRap8@dfpeg0tdy")
