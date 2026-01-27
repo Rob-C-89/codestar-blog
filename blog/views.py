5abcd8cf459e6b9636f92758f1630c1e28c9ba93
Rob-C-89 <robbiecornell@gmail.com>
2026-01-27 09:53:04 +0000

    Write Add comment function

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -49,6 +50,16 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
 
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+
+    comment_form = CommentForm()
+
     # Python functions always have a return. The path to
     # the template file is included in the view function return.
     # render() is a shortcut to load data to a template and return it.
@@ -71,5 +82,6 @@ def post_detail(request, slug):
             "post": post,
             "comments": comments,
             "comment_count": comment_count,
+            "comment_form": comment_form,
         }
     )
