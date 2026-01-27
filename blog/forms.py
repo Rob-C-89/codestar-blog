63ce5e7324ebe5b2166bf213a3ac4b7a758ce0c4
Rob-C-89 <robbiecornell@gmail.com>
2026-01-27 11:40:20 +0000

    Finish Collaboration function wtih POST handling

--- blog/forms.py
+++ blog/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 # forms.ModelForm is a built-in Django class.
 # As a result, we can use the Meta class to tell
-# he ModelForm class what models and fields we want in our form. 
+# he ModelForm class what models and fields we want in our form.
 # form.ModelForm will then build this for us.
 
 # We included the body field for the user to complete.
@@ -11,6 +11,7 @@ from django import forms
 # We didn't need to include the other fields because the post,
 # user and created_on fields in our model are filled in automatically,
 # and the approved field is managed by the superuser.
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
